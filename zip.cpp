d04b6d8b6af1b656497ca8f04234b08cff4859f7
xryuseix <ryusei143.shootingstar@gmail.com>
2019-11-01 23:28:07 +0900

    近い座標の画素をグループ化した

--- zip.cpp
+++ zip.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <set>
+#include <queue>
 using namespace std;
 
 vector<int> split(string& input, char delimiter)
@@ -31,14 +32,50 @@ int main(void){
     }
 
     // CSVのX座標，Y座標をpair形に変換
-    vector<pair<int, int> > point(grid[0].size());
+    set<pair<int, int> > point;
     for(int i = 0; i < grid[0].size(); i++) {
-        point[i] = make_pair(grid[0][i], grid[1][i]);
+        point.insert(make_pair(grid[0][i], grid[1][i]));
     }
-    sort(point.begin(), point.end());
 
-    // pointの集合を配列で持つ
-    vector<set<int> > group;
-    
+    // pointで選んだ要素の集合を配列で持つ
+    vector<set<pair<int, int> > > group;
+    while(point.size()>0) {
+        pair<int, int> p = *(point.begin());
+        point.erase(point.begin());
+
+        set<pair<int, int> > tmp;
+        tmp.insert(p);
+        
+        // 幅優先探索
+        queue<pair<int,int> > q;
+        q.push(p);
+        while(!q.empty()) {
+            pair<int, int> now = q.front();
+            q.pop();
+            // 移動方向の差分配列
+            int dx[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+            int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+            for(int i = 0; i < 8; i++) {
+                auto it = point.find(make_pair(now.first + dx[i], now.second + dy[i]));
+                if(it != point.end()) {
+                    q.push(*it);
+                    point.erase(it);
+                    tmp.insert(*it);
+                }
+            }
+        }
+        group.push_back(tmp);
+    }
+    // for(int i=0;i<group.size();i++){
+    //     cout<<endl;
+    //     for(int j=0;j<10;j++){
+    //         cout<<"----------------------------------"<<endl;
+    //     }
+    //     cout<<endl;
+    //     for(auto it=group[i].begin();it!=group[i].end();it++){
+    //         cout<<"("<<(*it).first <<","<<(*it).second<<") ";
+    //     }
+    //     cout<<endl;
+    // }
 
 }
