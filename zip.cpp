a84032986f176603a874ce1803d9145f0b538a7b
xryuseix <ryusei143.shootingstar@gmail.com>
2019-11-01 21:37:01 +0900

    ファイル読み込み処理作成

--- zip.cpp
+++ zip.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
+
+vector<int> split(string& input, char delimiter)
+{
+    istringstream stream(input);
+    string field;
+    vector<int> result;
+    while (getline(stream, field, delimiter)) {
+        result.push_back(stoi(field));
+    }
+    return result;
+}
+
 int main(void){
+
+    vector<vector<int> > grid;
+
+    ifstream ifs("pixel.csv");
+    string line;
+    while (getline(ifs, line)) {
+        vector<int> strvec = split(line, ',');
+        grid.push_back(strvec);
+        cout<<strvec.size()<<endl;
+    }
     
+
 }
